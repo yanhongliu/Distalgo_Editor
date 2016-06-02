4a3f47bef86e4ed72ec4e35543ae472f24d57a83
zhenjin wang <zhenjin.wang@stonybrook.edu>
2016-06-02 18:08:02 -0400

    only one running distalgo each time

--- express.js
+++ express.js
@@ -4,12 +4,13 @@ var express = require('express'),
     fs = require("fs"),
     bodyParser = require('body-parser'),
     child_process=require('child_process'),
-    pid=0;
+    pid=-1;
 
 app.use(bodyParser.json());
 app.use(express.static('distalgo'));
 app.use(express.static('ace_editor'));
 app.use(express.static('controller'));
+
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname+'/main.html'));
@@ -25,11 +26,13 @@ app.get('/killprocess', function(req, res){
 });
 
 app.post('/write', function(req, res){
-  var data=req.body['code'];
-  var exec = child_process.exec;
-  var cmd = 'python -m da distalgo/orig.da';
-
-         
+  if(pid!=-1){
+      res.send(JSON.stringify({'out':'Please stopping the last distalgo algorithm!'}));
+      return;
+  }
+  var data=req.body['code'],
+      exec = child_process.exec,
+      cmd = 'python -m da distalgo/orig.da';
   fs.writeFile('distalgo/orig.da',data,function(error){
      if(error){
          res.json({'content':error});
@@ -37,6 +40,7 @@ app.post('/write', function(req, res){
          console.log('exec');
          pro=exec(cmd,function(err,stdrr,stdout){
                                 console.log(stdout);
+                                pid=-1;
                                 res.send(JSON.stringify({'out':stdout}));
                     });
          pid=pro.pid;
@@ -44,5 +48,6 @@ app.post('/write', function(req, res){
   });
 });
 
-app.listen(3000);
-console.log('running '+3000);
+app.listen(3000,function(){
+    console.log('running '+3000);
+});
